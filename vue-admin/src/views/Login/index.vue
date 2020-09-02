915abbc52a9fb86ca9f566074133e601a84e7f4d
Yuting1007 <yutingdu830@outlook.com>
2020-09-02 15:31:17 -0500

    initiate the project

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "Login", current: true },
+        { txt: "Sign Up", current: false }
+      ],
+      isActive: true
+    };
+  },
+  create() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      data.current = true;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+}
+li {
+  display: inline-block;
+  width: 88px;
+  line-height: 36px;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 2px;
+  cursor: pointer;
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+</style>
