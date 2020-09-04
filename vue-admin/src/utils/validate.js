ef02d6318ecfa32155d8f646dc922c97890131fc
Yuting1007 <yutingdu830@outlook.com>
2020-09-04 00:28:59 -0500

    login form part2

--- vue-admin/src/utils/validate.js
+++ vue-admin/src/utils/validate.js
@@ -1,9 +1,20 @@
 export function validate_email(value) {
-  let regEmail = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+  let regEmail = /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
   return regEmail.test(value);
 }
 
 export function validate_password(value) {
   let regPassword = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
   return regPassword.test(value);
+}
+
+export function stripscript(s) {
+  let pattern = new RegExp(
+    "[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】 ‘；：”“'。，、？]"
+  );
+  let rs = "";
+  for (let i = 0; i < s.length; i++) {
+    rs = rs + s.substr(i, 1).replace(pattern, "");
+  }
+  return rs;
 }
